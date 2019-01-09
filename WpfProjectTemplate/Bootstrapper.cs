823e5f24bb3f6056f060bc92695de66ba01e8e99
Archie Espelimbergo Cortez <fdeu.aalmine@gmail.com>
2019-01-10 06:23:22 +0800

    Initial

---
+++ WpfProjectTemplate/Bootstrapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using Unity;
+using WpfProjectTemplate.ViewModels;
+
+namespace WpfProjectTemplate
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        private readonly IUnityContainer _container = new UnityContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+
+        protected override void Configure()
+        {
+            base.Configure();
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.ResolveAll(service);
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.Resolve(service, key);
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
