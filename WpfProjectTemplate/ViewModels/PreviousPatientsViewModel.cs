17d01cebf39bd204150c3a4d8c97ece7f6c4eade
Aqudei <fdeu.aalmine@gmail.com>
2019-03-03 20:39:07 +0800

    previous patient

--- WpfProjectTemplate/ViewModels/PreviousPatientsViewModel.cs
+++ WpfProjectTemplate/ViewModels/PreviousPatientsViewModel.cs
@@ -17,6 +17,7 @@ namespace OutPatientApp.ViewModels
         public ICollectionView Items { get; set; }
 
         private BindableCollection<Checkup> _checkups = new BindableCollection<Checkup>();
+        private string _searchText;
 
         public PreviousPatientsViewModel(LoginViewModel loginViewModel)
         {
@@ -33,6 +34,31 @@ namespace OutPatientApp.ViewModels
                 _checkups.AddRange(db.Checkups.Include("Patient")
                     .Where(c => c.DoctorId == _loginViewModel.Account.Id).ToList());
             }
+        }
+
+        public string SearchText
+        {
+            get => _searchText;
+            set => Set(ref _searchText, value);
+        }
+
+        public void DoFilter()
+        {
+            if (string.IsNullOrWhiteSpace(SearchText))
+            {
+                Items.Filter = o => true;
+                return;
+            }
+
+            var searchText = SearchText.ToLower();
+            Items.Filter = o =>
+            {
+                var checkup = o as Checkup;
+                return checkup.CaseNumber.Contains(searchText) ||
+                       checkup.Patient.FullName.ToLower().Contains(searchText) ||
+                       checkup.Complaint.ToLower().Contains(searchText) ||
+                       checkup.Diagnosis.ToLower().Contains(searchText);
+            };
         }
     }
 }
